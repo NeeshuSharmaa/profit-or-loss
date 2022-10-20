0f9fcef89227f0194d19c9987c0745ac976dbaa7
NeeshuSharmaa <neeshusharmaa@gmail.com>
2022-10-21 03:14:12 +0530

    Error correction: Non-negative values

--- app.js
+++ app.js
@@ -14,30 +14,36 @@ function calculateProfitOrLoss() {
 
     if (bPrice && currPrice && quant) {
         if(Number(bPrice) !="NaN" && Number(currPrice) !="NaN" && Number(quant) !="NaN"){
-            if (totalCPrice<totalBPrice) {
-                var loss = totalBPrice-totalCPrice;
-                var lossPercentage =loss/totalBPrice*100;
-                output.innerText ="Oops! You have a loss of Rs." + loss +". Loss Percentage: " + lossPercentage +"%";
+            if(Number(bPrice)>0 && Number(currPrice)>0 && Number(quant)>0){
+                if (totalCPrice<totalBPrice) {
+                    var loss = totalBPrice-totalCPrice;
+                    var lossPercentage =loss/totalBPrice*100;
+                    output.innerText ="Oops! You have a loss of Rs." + loss +". Loss Percentage: " + lossPercentage +"%";
+                }
+                else if(totalCPrice>totalBPrice) {
+                    var profit = totalCPrice-totalBPrice;
+                    var profitPercentage =profit/totalBPrice*100;
+                    output.innerText ="Hurray! You have a profit of Rs." + profit +". Profit Percentage: " + profitPercentage + "%";
+            
+                }
+                else if (totalCPrice=totalBPrice) {
+                    output.innerText="Oops! You don't have any loss or profit!"
+                }
             }
-            else if(totalCPrice>totalBPrice) {
-                var profit = totalCPrice-totalBPrice;
-                var profitPercentage =profit/totalBPrice*100;
-                output.innerText ="Hurray! You have a profit of Rs." + profit +". Profit Percentage: " + profitPercentage + "%";
-        
+            else{
+                output.innerText ="Kindly enter non-negative values only!!"
             }
-            else if (totalCPrice=totalBPrice) {
-                output.innerText="Oops! You don't have any loss or profit!"
-            }
+                
 
         } 
         
         else {
-            output.innerText =" Please input only numbers!!"
+            output.innerText =" Please input only numeric values!! →_→"
         }
 
     } 
     else {
-        output.innerText= "Please enter all the values!!"
+        output.innerText= "Please enter all the values!! -_-"
     }
 
 }
